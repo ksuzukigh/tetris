0e8581b7f631bcf80fdaadf4ab94e931627ee913
ksuzukigh <137459660+ksuzukigh@users.noreply.github.com>
2023-06-23 15:15:02 +0900

    Create tetris.js

---
+++ tetris.js
@@ -0,0 +1,39 @@
+const gameBoard = document.querySelector('#game-board');
+
+// ブロックの定義
+const blocks = [
+  {
+    color: 'cyan',
+    shape: [[1, 1, 1, 1]]
+  },
+  // 他のブロックをここに定義
+];
+
+let currentBlock = null;
+let currentX = 0;
+let currentY = 0;
+
+function startGame() {
+  // ランダムなブロックを選択
+  currentBlock = blocks[Math.floor(Math.random() * blocks.length)];
+  currentX = Math.floor(gameBoard.clientWidth / 30 / 2);
+  currentY = 0;
+  drawBlock();
+}
+
+function drawBlock() {
+  currentBlock.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value === 1) {
+        const blockElement = document.createElement('div');
+        blockElement.classList.add('block');
+        blockElement.style.backgroundColor = currentBlock.color;
+        blockElement.style.left = `${(currentX + x) * 30}px`;
+        blockElement.style.top = `${(currentY + y) * 30}px`;
+        gameBoard.appendChild(blockElement);
+      }
+    });
+  });
+}
+
+startGame();
