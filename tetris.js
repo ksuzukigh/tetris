98b8fcea96a46b8f620c668acd1cee3469a90c40
ksuzukigh <137459660+ksuzukigh@users.noreply.github.com>
2023-06-23 17:08:53 +0900

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -1,4 +1,4 @@
-const gameBoardElement = document.querySelector('#game-board');
+const gameBoard = document.querySelector('#game-board');
 
 // ブロックの定義
 const blocks = [
@@ -9,24 +9,18 @@ const blocks = [
   // 他のブロックをここに定義
 ];
 
-let gameBoard = [];
 let currentBlock = null;
 let currentX = 0;
 let currentY = 0;
 
 function startGame() {
-  // ゲームボードの状態を初期化
-  gameBoard = Array.from({ length: 20 }, () => Array(10).fill(0));
-  startNewBlock();
-  dropBlock();
-}
-
-function startNewBlock() {
   // ランダムなブロックを選択
   currentBlock = blocks[Math.floor(Math.random() * blocks.length)];
-  currentX = Math.floor(gameBoardElement.clientWidth / 30 / 2);
+  currentX = Math.floor(gameBoard.clientWidth / 30 / 2);
   currentY = 0;
   drawBlock();
+  // ゲーム開始時にブロックの落下を開始
+  dropBlock();
 }
 
 function drawBlock() {
@@ -41,7 +35,7 @@ function drawBlock() {
         blockElement.style.backgroundColor = currentBlock.color;
         blockElement.style.left = `${(currentX + x) * 30}px`;
         blockElement.style.top = `${(currentY + y) * 30}px`;
-        gameBoardElement.appendChild(blockElement);
+        gameBoard.appendChild(blockElement);
       }
     });
   });
@@ -50,35 +44,8 @@ function drawBlock() {
 function dropBlock() {
   currentY++;
   drawBlock();
-  // ブロックが底に到達したときの処理
-  if (hasReachedBottom()) {
-    copyBlockToGameBoard();
-    startNewBlock();
-  }
+  // 1秒ごとにブロックを下に移動
   setTimeout(dropBlock, 1000);
-}
-
-function hasReachedBottom() {
-  for (let y = 0; y < currentBlock.shape.length; y++) {
-    for (let x = 0; x < currentBlock.shape[y].length; x++) {
-      if (currentBlock.shape[y][x] === 1) {
-        if (currentY + y + 1 >= 20 || gameBoard[currentY + y + 1][currentX + x] !== 0) {
-          return true;
-        }
-      }
-    }
-  }
-  return false;
-}
-
-function copyBlockToGameBoard() {
-  for (let y = 0; y < currentBlock.shape.length; y++) {
-    for (let x = 0; x < currentBlock.shape[y].length; x++) {
-      if (currentBlock.shape[y][x] === 1) {
-        gameBoard[currentY + y][currentX + x] = 1;
-      }
-    }
-  }
 }
 
 startGame();
