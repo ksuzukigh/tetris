df0aa09dc60d28ce09f93222a99ae35aa22bbf1b
ksuzukigh <137459660+ksuzukigh@users.noreply.github.com>
2023-06-23 17:03:07 +0900

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -1,102 +1,84 @@
-let canvas;
-let ctx;
-let frames;
-let grid;
-let currentPos = { x: 0, y: 0 };
-let currentPiece;
-let pieces = [
-    [[1, 1, 1, 1]], // I
-    [[1, 1, 1, 0],  // L
-     [1, 0, 0, 0]],
-    [[1, 1, 1, 0],  // J
-     [0, 0, 1, 0]],
-    [[1, 1],        // O
-     [1, 1]],
-    [[1, 1, 0],     // S
-     [0, 1, 1]],
-    [[0, 1, 1],     // Z
-     [1, 1, 0]],
-    [[1, 1, 1],     // T
-     [0, 1, 0]]
+const gameBoardElement = document.querySelector('#game-board');
+
+// ブロックの定義
+const blocks = [
+  {
+    color: 'cyan',
+    shape: [[1, 1, 1, 1]]
+  },
+  // 他のブロックをここに定義
 ];
 
-function resetGrid() {
-    grid = Array.from({length: 20}, () => Array(10).fill(0));
-}
-
-function createPiece() {
-    currentPiece = pieces[Math.floor(Math.random() * pieces.length)];
-    currentPos = {x: 5 - Math.floor(currentPiece[0].length / 2), y: 0};
-}
-
-function drawPiece(piece, offset) {
-    piece.forEach((row, y) => {
-        row.forEach((value, x) => {
-            if (value !== 0) {
-                ctx.fillRect((offset.x + x) * 30, (offset.y + y) * 30, 30, 30);
-            }
-        });
-    });
-}
-
-function isValidMove(piece, offset) {
-    for (let y = 0; y < piece.length; y++) {
-        for (let x = 0; x < piece[y].length; x++) {
-            if (piece[y][x] !== 0 &&
-                (grid[y + offset.y] && grid[y + offset.y][x + offset.x]) !== 0) {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-function placePiece() {
-    for (let y = 0; y < currentPiece.length; y++) {
-        for (let x = 0; x < currentPiece[y].length; x++) {
-            if (currentPiece[y][x]) {
-                grid[currentPos.y + y][currentPos.x + x] = 1;
-            }
-        }
-    }
-}
-
-function update() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawPiece(currentPiece, currentPos);
-    frames++;
-
-    if (frames % 30 === 0) {
-        if (isValidMove(currentPiece, {x: currentPos.x, y: currentPos.y + 1})) {
-            currentPos.y++;
-        } else {
-            placePiece();
-            createPiece();
-        }
-    }
-    
-    requestAnimationFrame(update);
-}
-
-window.onkeydown = (e) => {
-    if (e.key === "ArrowLeft" && isValidMove(currentPiece, {x: currentPos.x - 1, y: currentPos.y})) {
-        currentPos.x--;
-    } else if (e.key === "ArrowRight" && isValidMove(currentPiece, {x: currentPos.x + 1, y: currentPos.y})) {
-        currentPos.x++;
-    } else if (e.key === "ArrowUp") {
-        rotatePiece();
-    } else if (e.key === " ") {
-        dropPiece();
-    }
-};
+let gameBoard = [];
+let currentBlock = null;
+let currentX = 0;
+let currentY = 0;
 
 function startGame() {
-    canvas = document.getElementById('board');
-    ctx = canvas.getContext('2d');
-    frames = 0;
-    resetGrid();
-    createPiece();
-    update();
+  // ゲームボードの状態を初期化
+  gameBoard = Array.from({ length: 20 }, () => Array(10).fill(0));
+  startNewBlock();
+  dropBlock();
+}
+
+function startNewBlock() {
+  // ランダムなブロックを選択
+  currentBlock = blocks[Math.floor(Math.random() * blocks.length)];
+  currentX = Math.floor(gameBoardElement.clientWidth / 30 / 2);
+  currentY = 0;
+  drawBlock();
+}
+
+function drawBlock() {
+  // すでに存在するブロックを削除
+  document.querySelectorAll('.block').forEach(block => block.remove());
+
+  currentBlock.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value === 1) {
+        const blockElement = document.createElement('div');
+        blockElement.classList.add('block');
+        blockElement.style.backgroundColor = currentBlock.color;
+        blockElement.style.left = `${(currentX + x) * 30}px`;
+        blockElement.style.top = `${(currentY + y) * 30}px`;
+        gameBoardElement.appendChild(blockElement);
+      }
+    });
+  });
+}
+
+function dropBlock() {
+  currentY++;
+  drawBlock();
+  // ブロックが底に到達したときの処理
+  if (hasReachedBottom()) {
+    copyBlockToGameBoard();
+    startNewBlock();
+  }
+  setTimeout(dropBlock, 1000);
+}
+
+function hasReachedBottom() {
+  for (let y = 0; y < currentBlock.shape.length; y++) {
+    for (let x = 0; x < currentBlock.shape[y].length; x++) {
+      if (currentBlock.shape[y][x] === 1) {
+        if (currentY + y + 1 >= 20 || gameBoard[currentY + y + 1][currentX + x] !== 0) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+function copyBlockToGameBoard() {
+  for (let y = 0; y < currentBlock.shape.length; y++) {
+    for (let x = 0; x < currentBlock.shape[y].length; x++) {
+      if (currentBlock.shape[y][x] === 1) {
+        gameBoard[currentY + y][currentX + x] = 1;
+      }
+    }
+  }
 }
 
 startGame();
