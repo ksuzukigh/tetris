e3dccf2d1799dda902ae8239b175ef081d99657b
ksuzukigh <137459660+ksuzukigh@users.noreply.github.com>
2023-06-24 20:14:39 +0900

    Update tetris-utils.js

--- tetris-utils.js
+++ tetris-utils.js
@@ -1,52 +1,93 @@
-class Piece {
-    constructor(matrix, color) {
-        this.matrix = matrix;
-        this.color = color;
-        this.pos = { y: 0, x: Math.floor(createBoard[0].length / 2) };
-    }
-}
-
-const pieces = [
-    [Z, "red"],
-    [S, "green"],
-    [T, "yellow"],
-    [O, "blue"],
-    [L, "pink"],
-    [I, "cyan"],
-    [J, "orange"]
+const I = [
+    [1, 1, 1, 1]
 ];
 
-function createBoard(w, h) {
-    let matrix = [];
-    while (h--) {
-        matrix.push(new Array(w).fill(0));
+const J = [
+    [1, 0, 0],
+    [1, 1, 1]
+];
+
+const L = [
+    [0, 0, 1],
+    [1, 1, 1]
+];
+
+const O = [
+    [1, 1],
+    [1, 1]
+];
+
+const S = [
+    [0, 1, 1],
+    [1, 1, 0]
+];
+
+const T = [
+    [0, 1, 0],
+    [1, 1, 1]
+];
+
+const Z = [
+    [1, 1, 0],
+    [0, 1, 1]
+];
+
+function createPiece(type) {
+    switch (type) {
+        case 'I':
+            return I;
+        case 'J':
+            return J;
+        case 'L':
+            return L;
+        case 'O':
+            return O;
+        case 'S':
+            return S;
+        case 'T':
+            return T;
+        case 'Z':
+            return Z;
     }
-    return matrix;
 }
 
-function getRandomPiece() {
-    let [p, c] = pieces[pieces.length * Math.random() | 0];
-    return new Piece(p, c);
-}
-
-function draw(board, context) {
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    context.fillStyle = 'white';
-    for (let y = 0; y < board.length; y++) {
-        for (let x = 0; x < board[y].length; x++) {
-            if (board[y][x]) {
-                context.fillRect(x, y, 1, 1);
+function drawPiece(piece, offset) {
+    piece.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                context.fillStyle = COLORS[value];
+                context.fillRect(x + offset.x,
+                    y + offset.y,
+                    1, 1);
             }
-        }
-    }
+        });
+    });
 }
 
-function collide(piece, board) {
-    const m = piece.matrix;
-    const o = piece.pos;
+function draw() {
+    context.fillStyle = '#000';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    drawPiece(player.matrix, player.pos);
+}
+
+function merge(arena, player) {
+    player.matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                arena[y + player.pos.y][x + player.pos.x] = value;
+            }
+        });
+    });
+}
+
+function collide(arena, player) {
+    const [m, o] = [player.matrix, player.pos];
     for (let y = 0; y < m.length; ++y) {
         for (let x = 0; x < m[y].length; ++x) {
-            if (m[y][x] !== 0 && (board[y + o.y] && board[y + o.y][x + o.x]) !== 0) {
+            if (m[y][x] !== 0 &&
+                (arena[y + o.y] &&
+                    arena[y + o.y][x + o.x]) !== 0) {
                 return true;
             }
         }
@@ -54,61 +95,22 @@ function collide(piece, board) {
     return false;
 }
 
-function merge(piece, board) {
-    piece.matrix.forEach((row, y) => {
-        row.forEach((value, x) => {
-            if (value !== 0) {
-                board[y + piece.pos.y][x + piece.pos.x] = value;
-            }
-        });
-    });
-}
-
 function rotate(matrix, dir) {
     for (let y = 0; y < matrix.length; ++y) {
         for (let x = 0; x < y; ++x) {
-            [matrix[x][y], matrix[y][x]] = [matrix[y][x], matrix[x][y]];
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                matrix[y][x],
+                matrix[x][y],
+            ];
         }
     }
+
     if (dir > 0) {
         matrix.forEach(row => row.reverse());
     } else {
         matrix.reverse();
-    }
-}
-
-function dropPiece(piece, board) {
-    piece.pos.y++;
-    if (collide(piece, board)) {
-        piece.pos.y--;
-        merge(piece, board);
-        piece = getRandomPiece();
-        board.fill(0);
-        if (collide(piece, board)) {
-            // game over
-            board.forEach(row => row.fill(0));
-        }
-    }
-}
-
-function move(dir, piece, board) {
-    piece.pos.x += dir;
-    if (collide(piece, board)) {
-        piece.pos.x -= dir;
-    }
-}
-
-function rotatePiece(dir, piece, board) {
-    const pos = piece.pos.x;
-    let offset = 1;
-    rotate(piece.matrix, dir);
-    while (collide(piece, board)) {
-        piece.pos.x += offset;
-        offset = -(offset + (offset > 0 ? 1 : -1));
-        if (offset > piece.matrix[0].length) {
-            rotate(piece.matrix, -dir);
-            piece.pos.x = pos;
-            return;
-        }
     }
 }
