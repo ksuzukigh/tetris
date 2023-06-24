31b522a3ed7ba186a5a6862e242e8358c0c4799c
ksuzukigh <137459660+ksuzukigh@users.noreply.github.com>
2023-06-24 13:56:20 +0900

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("game-board");
 const ctx = canvas.getContext('2d');
 
-const scale = 20;
+const scale = 40;
 const rows = canvas.height / scale;
 const columns = canvas.width / scale;
 
@@ -18,11 +18,13 @@ const shapes = {
   ],
   L: [
     [0, 1],
-    [1, 1, 1],
+    [0, 1],
+    [1, 1],
   ],
   J: [
     [1, 0],
-    [1, 1, 1],
+    [1, 0],
+    [1, 1],
   ],
   O: [
     [1, 1],
@@ -44,22 +46,23 @@ const shapes = {
 
 const colors = [
   null,
-  'red'
+  'red',
 ];
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw the game board
   board.forEach((row, y) => {
     row.forEach((value, x) => {
-      ctx.fillStyle = colors[value];
-      ctx.fillRect(x * scale, y * scale, scale, scale);
-      
-      // Draw grid
-      ctx.strokeStyle = '#ddd';
-      ctx.strokeRect(x * scale, y * scale, scale, scale);
+      if (value !== 0) {
+        ctx.fillStyle = colors[value];
+        ctx.fillRect(x * scale, y * scale, scale, scale);
+      }
     });
   });
 
+  // Draw the current piece
   if (currentPiece) {
     currentPiece.shape.forEach((row, y) => {
       row.forEach((value, x) => {
@@ -110,7 +113,7 @@ function dropPiece() {
     generatePiece();
     if (collision()) {
       // Game over
-      board = board.map(row => row.fill(0));
+      board = createBoard(rows, columns);
     }
   }
   draw();
