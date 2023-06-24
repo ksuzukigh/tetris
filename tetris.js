45db71aac62b813856bea2c2fd651b1474a07305
ksuzukigh <137459660+ksuzukigh@users.noreply.github.com>
2023-06-24 20:59:14 +0900

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -1,157 +1,139 @@
-const canvas = document.getElementById('board');
-const context = canvas.getContext('2d');
-context.scale(BLOCK_SIZE, BLOCK_SIZE);
+const canvas = document.getElementById("game-board");
+const ctx = canvas.getContext('2d');
+
+const scale = 40;
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
+
+let currentPiece;
+let board = createBoard(rows, columns);
+
+function createBoard(rows, columns) {
+  return Array.from({ length: rows }, () => Array(columns).fill(0));
+}
+
+const shapes = {
+  I: [
+    [1, 1, 1, 1],
+  ],
+  L: [
+    [0, 2],
+    [0, 2],
+    [2, 2],
+  ],
+  J: [
+    [3, 0],
+    [3, 0],
+    [3, 3],
+  ],
+  O: [
+    [4, 4],
+    [4, 4],
+  ],
+  T: [
+    [0, 5, 0],
+    [5, 5, 5],
+  ],
+  S: [
+    [0, 6, 6],
+    [6, 6, 0],
+  ],
+  Z: [
+    [7, 7, 0],
+    [0, 7, 7],
+  ]
+};
 
 const colors = [
   null,
-  'red',
-  'blue',
-  'violet',
-  'orange',
-  'green',
-  'cyan',
-  'pink'
+  'cyan',    // I
+  'blue',    // J
+  'orange',  // L
+  'yellow',  // O
+  'purple',  // T
+  'green',   // S
+  'red'      // Z
 ];
 
-// Generate pieces
-const pieces = 'ILJOTSZ';
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-function createPiece(type)
-{
-    if (type === 'T') {
-        return [
-            [0, 0, 0],
-            [1, 1, 1],
-            [0, 1, 0],
-        ];
-    } else if (type === 'O') {
-        return [
-            [2, 2],
-            [2, 2],
-        ];
-    } else if (type === 'L') {
-        return [
-            [0, 3, 0],
-            [0, 3, 0],
-            [0, 3, 3],
-        ];
-    } else if (type === 'J') {
-        return [
-            [0, 4, 0],
-            [0, 4, 0],
-            [4, 4, 0],
-        ];
-    } else if (type === 'I') {
-        return [
-            [0, 5, 0, 0],
-            [0, 5, 0, 0],
-            [0, 5, 0, 0],
-            [0, 5, 0, 0],
-        ];
-    } else if (type === 'S') {
-        return [
-            [0, 6, 6],
-            [6, 6, 0],
-            [0, 0, 0],
-        ];
-    } else if (type === 'Z') {
-        return [
-            [7, 7, 0],
-            [0, 7, 7],
-            [0, 0, 0],
-        ];
-    }
-}
+  // Draw the game board
+  board.forEach((row, y) => {
+    row.forEach((value, x) => {
+      // Fill in the cell if it is not empty
+      if (value !== 0) {
+        ctx.fillStyle = colors[value];
+        ctx.fillRect(x * scale, y * scale, scale, scale);
+      }
 
-function draw() 
-{ 
-  context.fillStyle = '#000';
-  context.fillRect(0, 0, canvas.width, canvas.height);
-
-  drawMatrix(arena, {x: 0, y: 0});
-  drawMatrix(player.matrix, player.pos);
-}
-
-function drawMatrix(matrix, offset) {
-    matrix.forEach((row, y) => {
-        row.forEach((value, x) => {
-            if (value !== 0) {
-                context.fillStyle = colors[value];
-                context.fillRect(x + offset.x,
-                                 y + offset.y,
-                                 1, 1);
-            }
-        });
+      // Draw grid lines
+      ctx.strokeStyle = '#DDD';
+      ctx.strokeRect(x * scale, y * scale, scale, scale);
     });
-}
+  });
 
-function merge(arena, player) {
-    player.matrix.forEach((row, y) => {
-        row.forEach((value, x) => {
-            if (value !== 0) {
-                arena[y + player.pos.y][x + player.pos.x] = value;
-            }
-        });
-    });
-}
-
-function playerDrop() {
-    player.pos.y++;
-    dropCounter = 0;
-}
-
-function playerMove(dir) {
-    player.pos.x += dir;
-}
-
-function playerRotate(dir) {
-    rotate(player.matrix, dir);
-}
-
-function rotate(matrix, dir) {
-    for (let y = 0; y < matrix.length; ++y) {
-        for (let x = 0; x < y; ++x) {
-            [
-                matrix[x][y],
-                matrix[y][x],
-            ] = [
-                matrix[y][x],
-                matrix[x][y],
-            ];
+  // Draw the current piece
+  if (currentPiece) {
+    currentPiece.shape.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value !== 0) {
+          ctx.fillStyle = colors[value];
+          ctx.fillRect((x + currentPiece.x) * scale, (y + currentPiece.y) * scale, scale, scale);
         }
-    }
-
-    if (dir > 0) {
-        matrix.forEach(row => row.reverse());
-    } else {
-        matrix.reverse();
-    }
+      });
+    });
+  }
 }
 
-function update() {
-    draw();
-    requestAnimationFrame(update);
+function mergePiece() {
+  currentPiece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        board[y + currentPiece.y][x + currentPiece.x] = value;
+      }
+    });
+  });
 }
 
-const arena = createMatrix(12, 20);
-
-const player = {
-    pos: {x: 0, y: 0},
-    matrix: createPiece(pieces[pieces.length * Math.random() | 0]),
-};
-
-document.addEventListener('keydown', event => {
-    if (event.keyCode === 37) {
-        playerMove(-1);
-    } else if (event.keyCode === 39) {
-        playerMove(1);
-    } else if (event.keyCode === 40) {
-        playerDrop();
-    } else if (event.keyCode === 81) {
-        playerRotate(-1);
-    } else if (event.keyCode === 87) {
-        playerRotate(1);
+function collision() {
+  for (let y = 0; y < currentPiece.shape.length; y++) {
+    for (let x = 0; x < currentPiece.shape[y].length; x++) {
+      if (
+        currentPiece.shape[y][x] !== 0 &&
+        (board[y + currentPiece.y] && board[y + currentPiece.y][x + currentPiece.x]) !== 0
+      ) {
+        return true;
+      }
     }
-});
+  }
+  return false;
+}
 
-update();
+function generatePiece() {
+  const pieces = 'ILJOTSZ';
+  const piece = pieces[Math.floor(Math.random() * pieces.length)];
+  currentPiece = { x: 5, y: 0, shape: shapes[piece] };
+}
+
+function dropPiece() {
+  currentPiece.y++;
+  if (collision()) {
+    currentPiece.y--;
+    mergePiece();
+    generatePiece();
+    if (collision()) {
+      // Game over
+      board = createBoard(rows, columns);
+    }
+  }
+  draw();
+  setTimeout(dropPiece, 1000);
+}
+
+function startGame() {
+  generatePiece();
+  dropPiece();
+}
+
+startGame();
