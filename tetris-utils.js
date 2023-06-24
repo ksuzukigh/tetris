b26e623669540a67c9ad2c1b5f12e4ea47444bf9
ksuzukigh <137459660+ksuzukigh@users.noreply.github.com>
2023-06-24 17:27:36 +0900

    Update tetris-utils.js

--- tetris-utils.js
+++ tetris-utils.js
@@ -1,72 +1,147 @@
-const scale = 20;
 const canvas = document.getElementById("game-board");
 const ctx = canvas.getContext('2d');
 
+const scale = 20;
 const rows = canvas.height / scale;
 const columns = canvas.width / scale;
 
-const shapes = {
-  I: [
-    [1, 1, 1, 1]
-  ],
-  L: [
-    [1, 0],
-    [1, 0],
-    [1, 1]
-  ],
-  J: [
-    [0, 1],
-    [0, 1],
-    [1, 1]
-  ],
-  O: [
-    [1, 1],
-    [1, 1]
-  ],
-  T: [
-    [1, 1, 1],
-    [0, 1, 0]
-  ],
-  S: [
-    [0, 1, 1],
-    [1, 1]
-  ],
-  Z: [
-    [1, 1],
-    [0, 1, 1]
-  ]
-};
-
-const colors = [
-  null,
-  '#D32F2F', // red
-  '#C2185B', // pink
-  '#7B1FA2', // purple
-  '#512DA8', // deep purple
-  '#303F9F', // indigo
-  '#1976D2', // blue
-  '#0288D1', // light blue
-  '#0097A7', // cyan
-  '#00796B', // teal
-  '#388E3C', // green
-  '#689F38', // light green
-  '#AFB42B', // lime
-  '#FBC02D', // yellow
-  '#FFA000', // amber
-  '#F57C00', // orange
-  '#E64A19'  // deep orange
-];
+let currentPiece;
+let board = createBoard(rows, columns);
 
 function createBoard(rows, columns) {
   return Array.from({ length: rows }, () => Array(columns).fill(0));
 }
 
-function getRandomPiece() {
-  const pieces = 'ILJOTSZ';
-  const piece = pieces[Math.floor(Math.random() * pieces.length)];
-  return shapes[piece];
+const shapes = {
+  I: [
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0]
+  ],
+  J: [
+    [0, 2, 0],
+    [0, 2, 0],
+    [2, 2, 0]
+  ],
+  L: [
+    [0, 3, 0],
+    [0, 3, 0],
+    [0, 3, 3]
+  ],
+  O: [
+    [4, 4],
+    [4, 4]
+  ],
+  S: [
+    [0, 5, 5],
+    [5, 5, 0],
+    [0, 0, 0]
+  ],
+  Z: [
+    [6, 6, 0],
+    [0, 6, 6],
+    [0, 0, 0]
+  ],
+  T: [
+    [7, 7, 7],
+    [0, 7, 0],
+    [0, 0, 0]
+  ]
+};
+
+const colors = [
+  'none',
+  'cyan',
+  'blue',
+  'orange',
+  'yellow',
+  'green',
+  'purple',
+  'red'
+];
+
+function collide(board, piece) {
+  for (let y = 0; y < piece.shape.length; y++) {
+    for (let x = 0; x < piece.shape[y].length; x++) {
+      if (piece.shape[y][x] !== 0 &&
+        (board[y + piece.pos.y] &&
+        board[y + piece.pos.y][x + piece.pos.x]) !== 0) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
 
-function createPiece() {
-  return { x: 3, y: 0, shape: getRandomPiece() };
+function merge(board, piece) {
+  piece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        board[y + piece.pos.y][x + piece.pos.x] = value;
+      }
+    });
+  });
+}
+
+function draw() {
+  board.forEach((row, y) => {
+    row.forEach((value, x) => {
+      ctx.fillStyle = colors[value];
+      ctx.fillRect(x, y, 1, 1);
+    });
+  });
+
+  if (currentPiece) {
+    currentPiece.shape.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value !== 0) {
+          ctx.fillStyle = colors[value];
+          ctx.fillRect(x + currentPiece.pos.x, y + currentPiece.pos.y, 1, 1);
+        }
+      });
+    });
+  }
+}
+
+function resetBoard() {
+  board = getEmptyBoard();
+  dropCounter = 0;
+  dropInterval = 1000;
+  lastDropTime = 0;
+  gameOver = false;
+}
+
+function getRandomPiece() {
+  const pieces = 'ILJOTSZ';
+  return createPiece(pieces[pieces.length * Math.random() | 0]);
+}
+
+function getEmptyBoard() {
+  return Array.from({length: ROWS}, () => Array(COLUMNS).fill(0));
+}
+
+function createPiece(type) {
+  if (type === 'I') {
+    return new Piece(SHAPES_I, 'cyan');
+  } else if (type === 'J') {
+    return new Piece(SHAPES_J, 'blue');
+  } else if (type === 'L') {
+    return new Piece(SHAPES_L, 'orange');
+  } else if (type === 'O') {
+    return new Piece(SHAPES_O, 'yellow');
+  } else if (type === 'S') {
+    return new Piece(SHAPES_S, 'green');
+  } else if (type === 'T') {
+    return new Piece(SHAPES_T, 'purple');
+  } else if (type === 'Z') {
+    return new Piece(SHAPES_Z, 'red');
+  }
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBoard();
+  drawPiece();
 }
