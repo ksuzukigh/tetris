872f435f0a13a547772cd2c5f936cdfb0eb37b75
ksuzukigh <137459660+ksuzukigh@users.noreply.github.com>
2023-06-25 10:06:34 +0900

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -3,6 +3,7 @@
 let currentPiece;
 let board = createBoard(rows, columns);
 
+// ピースをボードに結合する関数
 function mergePiece() {
   currentPiece.shape.forEach((row, y) => {
     row.forEach((value, x) => {
@@ -13,6 +14,7 @@ function mergePiece() {
   });
 }
 
+// ピースが衝突するかどうかを判定する関数
 function collision() {
   for (let y = 0; y < currentPiece.shape.length; y++) {
     for (let x = 0; x < currentPiece.shape[y].length; x++) {
@@ -27,12 +29,51 @@ function collision() {
   return false;
 }
 
-
+// ゲームを開始する関数
 function startGame() {
   generatePiece();
   dropPiece();
 }
 
-window.onload = function() {
-  startGame();
-};
+// キーボードの操作を処理する関数
+function handleKeyPress(event) {
+  const { keyCode } = event;
+
+  switch (keyCode) {
+    case 37: // 左矢印キー
+      currentPiece.x--;
+      if (collision()) {
+        currentPiece.x++;
+      }
+      break;
+
+    case 39: // 右矢印キー
+      currentPiece.x++;
+      if (collision()) {
+        currentPiece.x--;
+      }
+      break;
+
+    case 40: // 下矢印キー
+      currentPiece.y++;
+      if (collision()) {
+        currentPiece.y--;
+        mergePiece();
+        generatePiece();
+        if (collision()) {
+          // ゲームオーバー
+          board = createBoard(rows, columns);
+        }
+      }
+      break;
+
+    case 38: // 上矢印キー
+      // ピースの回転処理はここに追加する
+      break;
+  }
+}
+
+// キーボードの操作を監視する
+window.addEventListener('keydown', handleKeyPress);
+
+startGame();
