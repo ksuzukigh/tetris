3e726683babb65758495176a4b30681b42511e7a
ksuzukigh <137459660+ksuzukigh@users.noreply.github.com>
2023-06-25 13:40:20 +0900

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -1,92 +1,114 @@
-const canvas = document.getElementById('game-board');
-const context = canvas.getContext('2d');
-const scale = 20;
+const rows = 40;
+const columns = 10;
+const squareColors = ['white', 'red', 'blue', 'yellow', 'green', 'purple', 'cyan', 'orange'];
+const pieceColors = ['red', 'blue', 'yellow', 'green', 'purple', 'cyan', 'orange'];
+let currentPiece;
+let board = createBoard(rows, columns);
 
-const tetriminos = [
-  [
-    [1, 1, 1, 1]
-  ],
-  [
-    [1, 1, 1, 0],
-    [0, 0, 1, 0]
-  ],
-  [
-    [0, 0, 1, 0],
-    [1, 1, 1, 0]
-  ],
-  [
-    [0, 1, 1],
-    [1, 1]
-  ],
-  [
-    [1, 1],
-    [1, 1]
-  ],
-  [
-    [1, 1, 0],
-    [0, 1, 1]
-  ],
-  [
-    [1, 1, 1],
-    [0, 1, 0]
-  ]
-];
-
-function generatePiece() {
-  const id = Math.floor(Math.random() * tetriminos.length);
-  const shape = tetriminos[id];
-
-  return {
-    id: id,
-    shape: shape,
-    x: Math.floor(columns / 2) - Math.floor(shape[0].length / 2),
-    y: 0
-  };
-}
-
-function drawSquare(x, y, color) {
-  context.fillStyle = color;
-  context.fillRect(x * scale, y * scale, scale, scale);
-
-  context.strokeStyle = 'black';
-  context.strokeRect(x * scale, y * scale, scale, scale);
-}
-
-function drawPiece(piece) {
-  for (let y = 0; y < piece.shape.length; y++) {
-    for (let x = 0; x < piece.shape[y].length; x++) {
-      if (piece.shape[y][x] === 1) {
-        drawSquare(piece.x + x, piece.y + y, pieceColors[piece.id]);
+function collision() {
+  for (let y = 0; y < currentPiece.shape.length; y++) {
+    for (let x = 0; x < currentPiece.shape[y].length; x++) {
+      if (
+        currentPiece.shape[y][x] !== 0 &&
+        (board[y + currentPiece.y] && board[y + currentPiece.y][x + currentPiece.x]) !== 0
+      ) {
+        return true;
       }
     }
   }
+  return false;
 }
 
-function drawBoard(board) {
-  for (let y = 0; y < board.length; y++) {
-    for (let x = 0; x < board[y].length; x++) {
-      drawSquare(x, y, squareColors[board[y][x]]);
-    }
-  }
-}
-
-function erasePiece(piece) {
-  for (let y = 0; y < piece.shape.length; y++) {
-    for (let x = 0; x < piece.shape[y].length; x++) {
-      if (piece.shape[y][x] === 1) {
-        drawSquare(piece.x + x, piece.y + y, squareColors[0]);
+function mergePiece() {
+  currentPiece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        board[y + currentPiece.y][x + currentPiece.x] = value;
       }
+    });
+  });
+}
+
+function rotatePiece(piece) {
+  for (let y = 0; y < piece.length; y++) {
+    for (let x = 0; x < y; x++) {
+      [piece[x][y], piece[y][x]] = [piece[y][x], piece[x][y]];
     }
+  }
+  piece.forEach(row => row.reverse());
+  return piece;
+}
+
+function handleKeyPress(event) {
+  const { keyCode } = event;
+
+  switch (keyCode) {
+    case 37: // 左矢印キー
+      erasePiece(currentPiece);
+      currentPiece.x--;
+      if (collision()) {
+        currentPiece.x++;
+      }
+      drawPiece(currentPiece);
+      break;
+
+    case 39: // 右矢印キー
+      erasePiece(currentPiece);
+      currentPiece.x++;
+      if (collision()) {
+        currentPiece.x--;
+      }
+      drawPiece(currentPiece);
+      break;
+
+    case 40: // 下矢印キー
+      erasePiece(currentPiece);
+      currentPiece.y++;
+      if (collision()) {
+        currentPiece.y--;
+        mergePiece();
+        generatePiece();
+        if (collision()) {
+          // ゲームオーバー
+          board = createBoard(rows, columns);
+        }
+      }
+      drawPiece(currentPiece);
+      break;
+      
+    case 38: // 上矢印キー
+      erasePiece(currentPiece);
+      const originalShape = currentPiece.shape;
+      currentPiece.shape = rotatePiece([...currentPiece.shape]);
+      if (collision()) {
+        currentPiece.shape = originalShape;
+      }
+      drawPiece(currentPiece);
+      break;
   }
 }
 
-function createBoard(rows, columns) {
-  let board = [];
-  for (let y = 0; y < rows; y++) {
-    board[y] = [];
-    for (let x = 0; x < columns; x++) {
-      board[y][x] = 0;
+function dropPiece() {
+  erasePiece(currentPiece);
+  currentPiece.y++;
+  if (collision()) {
+    currentPiece.y--;
+    mergePiece();
+    generatePiece();
+    if (collision()) {
+      // ゲームオーバー
+      board = createBoard(rows, columns);
     }
   }
-  return board;
+  drawPiece(currentPiece);
+  drawBoard(board);
+  setTimeout(dropPiece, 1000);
 }
+
+function startGame() {
+  generatePiece();
+  dropPiece();
+}
+
+window.addEventListener("keydown", handleKeyPress);
+startGame();
