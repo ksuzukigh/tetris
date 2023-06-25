753938183c44aa7baa0dd9d874e8a2c3b7f3ff83
ksuzukigh <137459660+ksuzukigh@users.noreply.github.com>
2023-06-25 14:31:16 +0900

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -1,3 +1,4 @@
+// tetris.js
 let currentPiece;
 let board = createBoard(rows, columns);
 
@@ -25,24 +26,59 @@ function collision() {
   return false;
 }
 
-function dropPiece() {
-  currentPiece.y++;
-  if (collision()) {
-    currentPiece.y--;
-    mergePiece();
-    generatePiece();
-    if (collision()) {
-      // Game over
-      board = createBoard(rows, columns);
-    }
+// ピースを反時計回りに90度回転させる関数
+function rotatePiece(piece) {
+  const newPiece = piece[0].map((val, index) => piece.map(row => row[index])).reverse();
+  return newPiece;
+}
+
+// キーボードの操作を処理する関数
+function handleKeyPress(event) {
+  const { keyCode } = event;
+
+  switch (keyCode) {
+    case 37: // 左矢印キー
+      currentPiece.x--;
+      if (collision()) {
+        currentPiece.x++;
+      }
+      break;
+
+    case 39: // 右矢印キー
+      currentPiece.x++;
+      if (collision()) {
+        currentPiece.x--;
+      }
+      break;
+
+    case 40: // 下矢印キー
+      currentPiece.y++;
+      if (collision()) {
+        currentPiece.y--;
+        mergePiece();
+        generatePiece();
+        if (collision()) {
+          // ゲームオーバー
+          board = createBoard(rows, columns);
+        }
+      }
+      break;
+      
+    case 38: // 上矢印キー
+      const originalShape = currentPiece.shape;
+      currentPiece.shape = rotatePiece([...currentPiece.shape]);
+      if (collision()) {
+        currentPiece.shape = originalShape;
+      }
+      break;
   }
 }
 
-function generatePiece() {
-  const pieces = 'ILJOTSZ';
-  const piece = pieces[Math.floor(Math.random() * pieces.length)];
-  currentPiece = { x: 5, y: 0, shape: shapes[piece] };
+window.addEventListener("keydown", handleKeyPress);
+
+function startGame() {
+  generatePiece();
+  dropPiece();
 }
 
-window.dropPiece = dropPiece;
-window.generatePiece = generatePiece;
+startGame();
