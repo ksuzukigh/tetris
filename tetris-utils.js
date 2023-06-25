e79485d252d1d6405fb4b47ec1f80a46d3899017
ksuzukigh <137459660+ksuzukigh@users.noreply.github.com>
2023-06-25 20:54:16 +0900

    Update tetris-utils.js

--- tetris-utils.js
+++ tetris-utils.js
@@ -13,7 +13,32 @@ const shapes = {
   I: [
     [1, 1, 1, 1],
   ],
-  // ... omitted for brevity ...
+  L: [
+    [0, 2],
+    [0, 2],
+    [2, 2],
+  ],
+  J: [
+    [3, 0],
+    [3, 0],
+    [3, 3],
+  ],
+  O: [
+    [4, 4],
+    [4, 4],
+  ],
+  T: [
+    [0, 5, 0],
+    [5, 5, 5],
+  ],
+  S: [
+    [0, 6, 6],
+    [6, 6, 0],
+  ],
+  Z: [
+    [7, 7, 0],
+    [0, 7, 7],
+  ]
 };
 
 const colors = [
@@ -27,7 +52,69 @@ const colors = [
   'red'      // Z
 ];
 
-// ... omitted for brevity ...
+function createBoard(rows, columns) {
+  return Array.from({ length: rows }, () => Array(columns).fill(0));
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  board.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        ctx.fillStyle = colors[value];
+        ctx.fillRect(x * scale, y * scale, scale, scale);
+      }
+
+      ctx.strokeStyle = '#DDD';
+      ctx.strokeRect(x * scale, y * scale, scale, scale);
+    });
+  });
+
+  if (currentPiece) {
+    currentPiece.shape.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value !== 0) {
+          ctx.fillStyle = colors[value];
+          ctx.fillRect((x + currentPiece.x) * scale, (y + currentPiece.y) * scale, scale, scale);
+        }
+      });
+    });
+  }
+}
+
+function generatePiece() {
+  const pieces = 'ILJOTSZ';
+  const piece = pieces[Math.floor(Math.random() * pieces.length)];
+  currentPiece = { x: 5, y: 0, shape: shapes[piece] };
+}
+
+let currentPiece;
+let board = createBoard(rows, columns);
+
+function mergePiece() {
+  currentPiece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        board[y + currentPiece.y][x + currentPiece.x] = value;
+      }
+    });
+  });
+}
+
+function collision() {
+  for (let y = 0; y < currentPiece.shape.length; y++) {
+    for (let x = 0; x < currentPiece.shape[y].length; x++) {
+      if (
+        currentPiece.shape[y][x] !== 0 &&
+        (board[y + currentPiece.y] && board[y + currentPiece.y][x + currentPiece.x]) !== 0
+      ) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
 
 function removeFullRows() {
   for (let y = rows - 1; y >= 0; y--) {
@@ -45,13 +132,36 @@ function updateScore() {
 }
 
 function dropPiece() {
-  // ... omitted for brevity ...
-  removeFullRows();
+  currentPiece.y++;
+  if (collision()) {
+    currentPiece.y--;
+    mergePiece();
+    removeFullRows();
+    generatePiece();
+  }
   draw();
   setTimeout(dropPiece, 1000);
 }
 
-// ... omitted for brevity ...
+function handleKeyPress(event) {
+  switch (event.key) {
+    case 'ArrowUp':
+      currentPiece.shape = rotate(currentPiece.shape);
+      break;
+    case 'ArrowDown':
+      currentPiece.y++;
+      break;
+    case 'ArrowRight':
+      currentPiece.x++;
+      break;
+    case 'ArrowLeft':
+      currentPiece.x--;
+      break;
+  }
+  draw();
+}
+
+document.addEventListener('keydown', handleKeyPress);
 
 function startGame() {
   generatePiece();
