96f96ea860fbc888ae2988fbaf489c4ad6ae9fbb
ksuzukigh <137459660+ksuzukigh@users.noreply.github.com>
2023-06-25 14:17:14 +0900

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -1,4 +1,6 @@
-// tetris.js
+let currentPiece;
+let board = createBoard(rows, columns);
+
 function mergePiece() {
   currentPiece.shape.forEach((row, y) => {
     row.forEach((value, x) => {
@@ -23,62 +25,24 @@ function collision() {
   return false;
 }
 
-// ピースを反時計回りに90度回転させる関数
-function rotatePiece(piece) {
-  for (let y = 0; y < piece.length; y++) {
-    for (let x = 0; x < y; x++) {
-      [piece[x][y], piece[y][x]] = [piece[y][x], piece[x][y]];
+function dropPiece() {
+  currentPiece.y++;
+  if (collision()) {
+    currentPiece.y--;
+    mergePiece();
+    generatePiece();
+    if (collision()) {
+      // Game over
+      board = createBoard(rows, columns);
     }
   }
-  piece.forEach(row => row.reverse());
-  return piece;
 }
 
-// キーボードの操作を処理する関数
-function handleKeyPress(event) {
-  const { keyCode } = event;
-
-  switch (keyCode) {
-    case 37: // 左矢印キー
-      currentPiece.x--;
-      if (collision()) {
-        currentPiece.x++;
-      }
-      break;
-
-    case 39: // 右矢印キー
-      currentPiece.x++;
-      if (collision()) {
-        currentPiece.x--;
-      }
-      break;
-
-    case 40: // 下矢印キー
-      currentPiece.y++;
-      if (collision()) {
-        currentPiece.y--;
-        mergePiece();
-        generatePiece();
-        if (collision()) {
-          // ゲームオーバー
-          board = createBoard(rows, columns);
-        }
-      }
-      break;
-      
-    case 38: // 上矢印キー
-      const originalShape = currentPiece.shape;
-      currentPiece.shape = rotatePiece([...currentPiece.shape]);
-      if (collision()) {
-        currentPiece.shape = originalShape;
-      }
-      break;
-  }
+function generatePiece() {
+  const pieces = 'ILJOTSZ';
+  const piece = pieces[Math.floor(Math.random() * pieces.length)];
+  currentPiece = { x: 5, y: 0, shape: shapes[piece] };
 }
 
-window.addEventListener("keydown", handleKeyPress);
-
-window.mergePiece = mergePiece;
-window.collision = collision;
-window.rotatePiece = rotatePiece;
-window.handleKeyPress = handleKeyPress;
+window.dropPiece = dropPiece;
+window.generatePiece = generatePiece;
