7ccf89fcbf797ac1ba70a77cec01cc5e965309f6
ksuzukigh <137459660+ksuzukigh@users.noreply.github.com>
2023-06-25 14:16:41 +0900

    Update tetris-utils.js

--- tetris-utils.js
+++ tetris-utils.js
@@ -1,133 +1,97 @@
-// tetris-utils.js
-// ゲームの設定
-const rows = 20;
-const columns = 10;
-const squareSize = 20;
+const canvas = document.getElementById("game-board");
+const ctx = canvas.getContext('2d');
 
-// ピースの定義
-const pieces = [
-  [
-    [0, 0, 0, 0],
+const scale = 40;
+
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
+
+const shapes = {
+  I: [
     [1, 1, 1, 1],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0]
   ],
-  [
-    [2, 0, 0],
-    [2, 2, 2],
-    [0, 0, 0]
+  L: [
+    [0, 2],
+    [0, 2],
+    [2, 2],
   ],
-  [
-    [0, 0, 3],
-    [3, 3, 3],
-    [0, 0, 0]
+  J: [
+    [3, 0],
+    [3, 0],
+    [3, 3],
   ],
-  [
+  O: [
     [4, 4],
-    [4, 4]
+    [4, 4],
   ],
-  [
-    [0, 5, 5],
-    [5, 5, 0],
-    [0, 0, 0]
+  T: [
+    [0, 5, 0],
+    [5, 5, 5],
   ],
-  [
-    [6, 6, 0],
+  S: [
     [0, 6, 6],
-    [0, 0, 0]
+    [6, 6, 0],
   ],
-  [
-    [0, 7, 0],
-    [7, 7, 7],
-    [0, 0, 0]
+  Z: [
+    [7, 7, 0],
+    [0, 7, 7],
   ]
+};
+
+
+const colors = [
+  null,
+  'cyan',    // I
+  'blue',    // J
+  'orange',  // L
+  'yellow',  // O
+  'purple',  // T
+  'green',   // S
+  'red'      // Z
 ];
 
-// ゲームボードの作成
 function createBoard(rows, columns) {
-  let board = [];
-  for (let row = 0; row < rows; row++) {
-    board[row] = [];
-    for (let column = 0; column < columns; column++) {
-      board[row][column] = 0;
-    }
-  }
-  return board;
+  return Array.from({ length: rows }, () => Array(columns).fill(0));
 }
 
-// キャンバスの設定
-let canvas = document.getElementById('game-board');
-let context = canvas.getContext('2d');
-context.scale(squareSize, squareSize);
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-// ピースの描画
-function drawPiece(x, y, shape) {
-  shape.forEach((row, dy) => {
-    row.forEach((value, dx) => {
-      if (value !== 0) {
-        context.fillStyle = colors[value];
-        context.fillRect(x + dx, y + dy, 1, 1);
-      }
-    });
-  });
-}
-
-// ボードの描画
-function drawBoard() {
-  context.fillStyle = '#000';
-  context.fillRect(0, 0, canvas.width, canvas.height);
+  // Draw the game board
   board.forEach((row, y) => {
     row.forEach((value, x) => {
-      context.fillStyle = colors[value];
-      context.fillRect(x, y, 1, 1);
+      // Fill in the cell if it is not empty
+      if (value !== 0) {
+        ctx.fillStyle = colors[value];
+        ctx.fillRect(x * scale, y * scale, scale, scale);
+      }
+
+      // Draw grid lines
+      ctx.strokeStyle = '#DDD';
+      ctx.strokeRect(x * scale, y * scale, scale, scale);
     });
   });
-  drawPiece(currentPiece.x, currentPiece.y, currentPiece.shape);
-}
 
-// ピースの生成
-function generatePiece() {
-  let index = Math.floor(Math.random() * pieces.length);
-  let shape = pieces[index];
-  currentPiece = {
-    x: Math.floor(columns / 2) - Math.floor(shape[0].length / 2),
-    y: 0,
-    shape: shape
-  };
-}
-
-// ピースの落下
-function dropPiece() {
-  currentPiece.y++;
-  if (collision()) {
-    currentPiece.y--;
-    mergePiece();
-    generatePiece();
-    if (collision()) {
-      // ゲームオーバー
-      board = createBoard(rows, columns);
-    }
+  // Draw the current piece
+  if (currentPiece) {
+    currentPiece.shape.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value !== 0) {
+          ctx.fillStyle = colors[value];
+          ctx.fillRect((x + currentPiece.x) * scale, (y + currentPiece.y) * scale, scale, scale);
+        }
+      });
+    });
   }
 }
 
-// 描画更新
 function update() {
-  drawBoard();
+  draw();
   dropPiece();
   requestAnimationFrame(update);
 }
 
-window.rows = rows;
-window.columns = columns;
-window.board = createBoard(rows, columns);
-window.currentPiece = undefined;
-window.colors = ['black', 'purple', 'yellow', 'orange', 'blue', 'aqua', 'green', 'red'];
-window.createBoard = createBoard;
-window.drawPiece = drawPiece;
-window.drawBoard = drawBoard;
-window.generatePiece = generatePiece;
-window.dropPiece = dropPiece;
 window.update = update;
 
-update();
 generatePiece();
+update();
